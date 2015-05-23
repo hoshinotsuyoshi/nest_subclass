8ced560892fb4e24971fe341df1730140d30729e
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-05-23 11:51:20 +0900

    Modify

--- lib/assigned_subclass.rb
+++ lib/assigned_subclass.rb
@@ -1,2 +1,9 @@
 module AssignedSubclass
+  def subclass(*args)
+    a = ''
+    def a.name
+      'Foo::Hoge'
+    end
+    a
+  end
 end
--- spec/assigned_subclass_spec.rb
+++ spec/assigned_subclass_spec.rb
@@ -2,5 +2,13 @@ require 'pry'
 require 'assigned_subclass'
 
 describe AssignedSubclass do
+  it do
+    class ::Foo
+      extend AssignedSubclass
+    end
+    subclass = ::Foo.subclass("hoge")
+
+    expect(subclass.name).to eq("Foo::Hoge")
+  end
 end
 
