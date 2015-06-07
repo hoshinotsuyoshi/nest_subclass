8f5a0678a95ee63e9d6d7b511e9738f208b56c9e
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-06-07 15:45:48 +0900

    Use camelize

--- lib/nest_subclass.rb
+++ lib/nest_subclass.rb
@@ -1,8 +1,10 @@
 require "nest_subclass/version"
+require "active_support"
+require "active_support/core_ext/string/inflections"
 
 module NestSubclass
   def subclass(name, &block)
     c = Class.new(self, &block)
-    self.const_set(:"#{name.capitalize}", c)
+    self.const_set(:"#{name.camelize}", c)
   end
 end
